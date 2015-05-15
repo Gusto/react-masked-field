478a43dee9c61ddbd4ac50d30e3ce801f388317f
Rylan Collins <rylan@zenpayroll.com>
2015-05-14 18:33:58 -0700

    Add latest dist files

--- dist/masked_input.js
+++ dist/masked_input.js
@@ -23,39 +23,51 @@ MaskedInput = React.createClass({
     if (this.props.mask == null) {
       return null;
     }
-    this.buffer = (function() {
+    this._buffer = (function() {
       var _i, _len, _ref, _results;
-      _ref = this.props.mask.split('');
+      _ref = this.props.mask;
       _results = [];
       for (idx = _i = 0, _len = _ref.length; _i < _len; idx = ++_i) {
         char = _ref[idx];
-        if (this.translations[char]) {
-          if (this.firstNonMaskIdx == null) {
-            this.firstNonMaskIdx = idx;
+        if (this._getPattern(idx)) {
+          if (this._firstNonMaskIdx == null) {
+            this._firstNonMaskIdx = idx;
           }
-          _results.push(this.getFormat(idx));
+          _results.push(this._getFormatChar(idx));
         } else {
           _results.push(char);
         }
       }
       return _results;
     }).call(this);
-    this.cursorPos = this.firstNonMaskIdx;
+    this._cursorPos = this._firstNonMaskIdx;
     return {
-      value: this.buffer.join('')
+      value: this._buffer.join('')
     };
   },
   componentDidUpdate: function() {
-    if (this.cursorPos != null) {
-      return this.setCursorPos(this.cursorPos);
+    if (this._cursorPos != null) {
+      return this._setSelection(this._cursorPos);
     }
   },
-  translations: {
+  render: function() {
+    var props;
+    props = {};
+    if (this.props.mask != null) {
+      props.onChange = this._handleChange;
+      props.onKeyPress = this._handleKeyPress;
+      props.onKeyDown = this._handleKeyDown;
+      props.onFocus = this._handleFocus;
+      props.value = (this.props.value != null) && this.props.value !== this.state.value ? this._maskedValue(this.props.value) : this.state.value;
+    }
+    return React.createElement("input", React.__spread({}, this.props, props));
+  },
+  _translations: {
     '9': /\d/,
     'a': /[A-Za-z]/,
     '*': /[A-Za-z0-9]/
   },
-  getCursorPos: function() {
+  _getSelection: function() {
     var begin, end, node, range;
     node = this.getDOMNode();
     if (node.setSelectionRange != null) {
@@ -71,7 +83,7 @@ MaskedInput = React.createClass({
       end: end
     };
   },
-  setCursorPos: function(begin, end) {
+  _setSelection: function(begin, end) {
     var node, range;
     if (end == null) {
       end = begin;
@@ -87,64 +99,64 @@ MaskedInput = React.createClass({
       return range.select();
     }
   },
-  getPattern: function(idx) {
-    return this.translations[this.props.mask[idx]];
+  _getPattern: function(idx) {
+    return this._translations[this.props.mask[idx]];
   },
-  getFormat: function(idx) {
+  _getFormatChar: function(idx) {
     idx = idx < this.props.format.length ? idx : 0;
     return this.props.format[idx];
   },
-  clearBuffer: function(start, end) {
+  _resetBuffer: function(start, end) {
     var i, _i;
     for (i = _i = start; start <= end ? _i < end : _i > end; i = start <= end ? ++_i : --_i) {
-      if (this.getPattern(i) != null) {
-        this.buffer[i] = this.getFormat(i);
+      if (this._getPattern(i) != null) {
+        this._buffer[i] = this._getFormatChar(i);
       }
     }
     return true;
   },
-  seekNext: function(pos) {
-    while (++pos < this.props.mask.length && (this.getPattern(pos) == null)) {
+  _seekNext: function(pos) {
+    while (++pos < this.props.mask.length && (this._getPattern(pos) == null)) {
       true;
     }
     return pos;
   },
-  seekPrev: function(pos) {
-    while (--pos >= 0 && (this.getPattern(pos) == null)) {
+  _seekPrev: function(pos) {
+    while (--pos >= 0 && (this._getPattern(pos) == null)) {
       true;
     }
     return pos;
   },
-  shiftLeft: function(begin, end) {
+  _shiftLeft: function(begin, end) {
     var i, next, pattern, _i, _ref;
-    this.clearBuffer(begin, end);
+    this._resetBuffer(begin, end);
     if (begin < 0) {
       return;
     }
-    next = this.seekNext(end - 1);
+    next = this._seekNext(end - 1);
     for (i = _i = begin, _ref = this.props.mask.length; begin <= _ref ? _i < _ref : _i > _ref; i = begin <= _ref ? ++_i : --_i) {
-      pattern = this.getPattern(i);
+      pattern = this._getPattern(i);
       if (pattern != null) {
-        if (next < this.props.mask.length && pattern.test(this.buffer[next])) {
-          this.buffer[i] = this.buffer[next];
-          this.buffer[next] = this.getFormat(next);
+        if (next < this.props.mask.length && pattern.test(this._buffer[next])) {
+          this._buffer[i] = this._buffer[next];
+          this._buffer[next] = this._getFormatChar(next);
         } else {
           break;
         }
-        next = this.seekNext(next);
+        next = this._seekNext(next);
       }
     }
-    return this.cursorPos = Math.max(begin, this.firstNonMaskIdx);
+    return this._cursorPos = Math.max(begin, this._firstNonMaskIdx);
   },
-  shiftRight: function(pos) {
+  _shiftRight: function(pos) {
     var c, i, next, pattern, t, _i, _ref;
-    c = this.getFormat(pos);
+    c = this._getFormatChar(pos);
     for (i = _i = pos, _ref = this.props.mask.length; pos <= _ref ? _i < _ref : _i > _ref; i = pos <= _ref ? ++_i : --_i) {
-      pattern = this.getPattern(i);
+      pattern = this._getPattern(i);
       if (pattern != null) {
-        next = this.seekNext(i);
-        t = this.buffer[i];
-        this.buffer[i] = c;
+        next = this._seekNext(i);
+        t = this._buffer[i];
+        this._buffer[i] = c;
         if (next < this.props.mask.length && pattern.test(t)) {
           c = t;
         } else {
@@ -154,19 +166,19 @@ MaskedInput = React.createClass({
     }
     return true;
   },
-  fireOnComplete: function(value) {
+  _callOnComplete: function(value) {
     var i, _i, _ref;
     if (this.props.onComplete == null) {
       return;
     }
     for (i = _i = 0, _ref = this.props.mask.length; 0 <= _ref ? _i < _ref : _i > _ref; i = 0 <= _ref ? ++_i : --_i) {
-      if ((this.getPattern(i) != null) && this.buffer[i] === this.getFormat(i)) {
+      if ((this._getPattern(i) != null) && this._buffer[i] === this._getFormatChar(i)) {
         return;
       }
     }
     return this.props.onComplete(value);
   },
-  setValue: function(value) {
+  _setValue: function(value) {
     var _base;
     if (typeof (_base = this.props).onChange === "function") {
       _base.onChange({
@@ -180,104 +192,91 @@ MaskedInput = React.createClass({
     });
     return value;
   },
-  handleFocus: function(e) {
+  _handleFocus: function(e) {
     var _base;
     setTimeout((function(_this) {
       return function() {
-        return _this.setCursorPos(_this.cursorPos);
+        return _this._setSelection(_this._cursorPos);
       };
     })(this), 0);
     return typeof (_base = this.props).onFocus === "function" ? _base.onFocus(e) : void 0;
   },
-  handleKeyDown: function(e) {
+  _handleKeyDown: function(e) {
     var begin, end, _base, _ref;
     if (e.key === 'Backspace' || e.key === 'Delete') {
-      _ref = this.getCursorPos(), begin = _ref.begin, end = _ref.end;
+      _ref = this._getSelection(), begin = _ref.begin, end = _ref.end;
       if (begin === end) {
         if (e.key === 'Delete') {
-          begin = this.seekNext(begin - 1);
-          end = this.seekNext(begin);
+          begin = this._seekNext(begin - 1);
+          end = this._seekNext(begin);
         } else {
-          begin = this.seekPrev(begin);
+          begin = this._seekPrev(begin);
         }
       }
-      this.shiftLeft(begin, end);
-      this.setValue(this.buffer.join(''));
+      this._shiftLeft(begin, end);
+      this._setValue(this._buffer.join(''));
       e.preventDefault();
     }
     return typeof (_base = this.props).onKeyDown === "function" ? _base.onKeyDown(e) : void 0;
   },
-  handleKeyPress: function(e) {
+  _handleKeyPress: function(e) {
     var begin, bufferChanged, end, next, value, _base, _ref;
     if (e.key.length === 1) {
-      _ref = this.getCursorPos(), begin = _ref.begin, end = _ref.end;
-      this.cursorPos = begin;
+      _ref = this._getSelection(), begin = _ref.begin, end = _ref.end;
+      this._cursorPos = begin;
       bufferChanged = false;
       if (begin !== end) {
-        this.shiftLeft(begin, end);
+        this._shiftLeft(begin, end);
         bufferChanged = true;
       }
-      next = this.seekNext(this.cursorPos - 1);
-      if (next < this.props.mask.length && this.getPattern(next).test(e.key)) {
-        this.shiftRight(next);
-        this.buffer[next] = e.key;
-        this.cursorPos = this.seekNext(next);
-        value = this.setValue(this.buffer.join(''));
-        this.fireOnComplete(value);
+      next = this._seekNext(this._cursorPos - 1);
+      if (next < this.props.mask.length && this._getPattern(next).test(e.key)) {
+        this._shiftRight(next);
+        this._buffer[next] = e.key;
+        this._cursorPos = this._seekNext(next);
+        value = this._setValue(this._buffer.join(''));
+        this._callOnComplete(value);
       } else if (bufferChanged) {
-        this.setValue(this.buffer.join(''));
+        this._setValue(this._buffer.join(''));
       }
       e.preventDefault();
     }
     return typeof (_base = this.props).onKeyPress === "function" ? _base.onKeyPress(e) : void 0;
   },
-  handleChange: function(e) {
+  _handleChange: function(e) {
     var value;
-    value = this.maskedValue(e.target.value);
-    this.setValue(value);
-    return this.fireOnComplete(value);
+    value = this._maskedValue(e.target.value);
+    this._setValue(value);
+    return this._callOnComplete(value);
   },
-  maskedValue: function(input) {
+  _maskedValue: function(input) {
     var c, i, pattern, pos, _i, _ref;
     pos = 0;
     for (i = _i = 0, _ref = this.props.mask.length; 0 <= _ref ? _i < _ref : _i > _ref; i = 0 <= _ref ? ++_i : --_i) {
-      pattern = this.getPattern(i);
+      pattern = this._getPattern(i);
       if (pattern != null) {
-        this.buffer[i] = this.getFormat(i);
+        this._buffer[i] = this._getFormatChar(i);
         while (pos++ < input.length) {
           c = input[pos - 1];
           if (pattern.test(c)) {
-            this.buffer[i] = c;
+            this._buffer[i] = c;
             break;
           }
         }
         if (pos > input.length) {
-          this.clearBuffer(i + 1, this.props.mask.length);
+          this._resetBuffer(i + 1, this.props.mask.length);
           break;
         }
-      } else if (this.buffer[i] === input[pos]) {
+      } else if (this._buffer[i] === input[pos]) {
         pos++;
       }
     }
-    this.cursorPos = i;
-    return this.buffer.join('');
-  },
-  render: function() {
-    var props;
-    props = {};
-    if (this.props.mask != null) {
-      props.onChange = this.handleChange;
-      props.onKeyPress = this.handleKeyPress;
-      props.onKeyDown = this.handleKeyDown;
-      props.onFocus = this.handleFocus;
-      props.value = (this.props.value != null) && this.props.value !== this.state.value ? this.maskedValue(this.props.value) : this.state.value;
-    }
-    return React.createElement("input", React.__spread({}, this.props, props));
+    this._cursorPos = i;
+    return this._buffer.join('');
   }
 });
 
 module.exports = MaskedInput;
-
 
 
 },{"react":"react"}]},{},[1])(1)
