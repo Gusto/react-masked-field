7d7561ae23a7553fd2a18da2f4a94ea579c6fb7a
Rylan Collins <rylan@zenpayroll.com>
2015-07-01 14:24:50 -0700

    Cleanup and browserify

--- js/main.js
+++ js/main.js
@@ -1,23 +1,28 @@
-$(document).ready(function(){
+'use strict';
 
-	$('li.tab').bind('click',function(){
-			$('.project-detail').each(function(){
-			$(this).addClass('hidden');
-	});
-			
-	var id = $(this).attr('id');
-			$('#project-'+id).removeClass('hidden');
-	});
+var React = require('react');
+var $ = window.$;
+var MaskedField = require('react-masked-field');
+var Project1 = require('./react-masked-field');
 
-	var MaskedField = window.MaskedField;
+$(document).ready(function() {
+  $('li.tab').bind('click', function() {
+    $('.project-detail').each(function() {
+      $(this).addClass('hidden');
+    });
 
-	function handleComplete(date) {
-	  console.log('Date is ' + date);
-	}
+    var id = $(this).attr('id');
+    $('#project-' + id).removeClass('hidden');
+  });
 
-	React.render(
-	  React.createElement(MaskedField, {mask: '99/99/9999'}),
-	  $('.react-masked-field-demo')[0]
-	);
+  function handleComplete(date) {
+    console.log('Date is ' + date);
+  }
 
+  React.render(
+		<MaskedField mask="99/99/9999" />,
+    $('.react-masked-field-demo')[0]
+  );
+
+  React.render(<Project1/>, $('.project-description')[0]);
 });
--- js/react-masked-field.js
+++ js/react-masked-field.js
@@ -1,16 +1,22 @@
+'use strict';
+
+var React = require('react');
+
 var Project1 = React.createClass({
 	getDefaultProps: function () {
-		return {project_title: "About React-Masked-Field",
-						project_description: "The MaskedField component is a text input field that allows you to restrict and format the values that can be entered into it, while informing the user of the expected input. Common uses include dates, phone numbers, social security numbers and tax IDs."};
+		return {
+			project_title: 'About React-Masked-Field',
+			project_description: 'The MaskedField component is a text input field that allows you to restrict and format the values that can be entered into it, while informing the user of the expected input. Common uses include dates, phone numbers, social security numbers and tax IDs.'
+		};
 	},
 	render: function () {
 		return (
 			<div>
-				<h4>{this.props.project_title}</h4>
+				<h3>{this.props.project_title}</h3>
 				<p>{this.props.project_description}</p>
 			</div>
 		);
 	}
 });
 
-React.renderComponent(<Project1/>, null, document.getElementById('project-description'));
+module.exports = Project1;
