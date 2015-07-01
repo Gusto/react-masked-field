0bfb6438fdfddc38a581538d9cc130170a7e8e43
Rylan Collins <rylan@zenpayroll.com>
2015-07-01 14:02:27 -0700

    Add browserify, babel and eslint

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,10 @@
+'use strict';
+
 module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-haml');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-browserify');
 
     grunt.initConfig({
         haml: {
@@ -26,6 +29,15 @@ module.exports = function (grunt) {
                 }]
             }
         },
+        browserify: {
+          app: {
+            src: 'js/main.js',
+            dest: 'js/app.js',
+            options: {
+              watch: true
+            }
+          }
+        },
         watch: {
             haml: {
                 files: ['**/*.haml'],
@@ -38,5 +50,5 @@ module.exports = function (grunt) {
         }
     });
 
-    grunt.registerTask('default', ['watch']);
+    grunt.registerTask('default', ['browserify', 'watch']);
 };
