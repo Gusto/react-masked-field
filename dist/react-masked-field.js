d59c8d4d244e390270694989d1eef80712d1a291
Rylan Collins <rylan@zenpayroll.com>
2015-08-17 17:50:09 -0700

    Version 0.1.4

--- dist/react-masked-field.js
+++ dist/react-masked-field.js
@@ -1,5 +1,5 @@
 /**
-* react-masked-field 0.1.3
+* react-masked-field 0.1.4
 */
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.MaskedField = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 /**
@@ -85,7 +85,7 @@ var MaskedField = React.createClass({
       }
     }
 
-    return React.createElement('input', _extends({}, this.props, props, { type: "text" }));
+    return React.createElement('input', _extends({}, this.props, props, { type: 'text' }));
   },
   _getSelection: function _getSelection() {
     if (this.isMounted()) {
@@ -98,7 +98,10 @@ var MaskedField = React.createClass({
   _setSelection: function _setSelection(start) {
     var end = arguments.length <= 1 || arguments[1] === undefined ? start : arguments[1];
     return (function () {
-      setSelection(this.getDOMNode(), start, end);
+      var domNode = React.findDOMNode(this);
+      if (domNode === document.activeElement) {
+        setSelection(domNode, start, end);
+      }
     }).apply(this, arguments);
   },
   _getPropsValue: function _getPropsValue() {
