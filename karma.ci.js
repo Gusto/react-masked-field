661b1e5d805daaf7146305748b22f4678ad8237a
Rylan Collins <rylan@zenpayroll.com>
2016-03-07 11:34:12 -0800

    Fix CI build

--- karma.ci.js
+++ karma.ci.js
@@ -24,7 +24,14 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome', 'Firefox'],
+    browsers: ['ChromeTravisCI', 'Firefox'],
+
+    customLaunchers: {
+      ChromeTravisCI: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    },
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
